105894b6a3e0885704daa43bac9be6e1d0af1aae
Maruf Tamboli <maruftamboli1@gmail.com>
2024-03-14 14:49:52 +0530

    first commit

---
+++ style-switcher.js
@@ -0,0 +1,47 @@
+// ======================Toggler style switcher=======================
+const styleSwitcherToggler = document.querySelector(".style-switchers-toggler");
+styleSwitcherToggler.addEventListener('click', () =>{
+    document.querySelector(".style-switchers").classList.toggle("open");
+})
+
+//hide style switcher on scroll
+window.addEventListener('scroll',() =>{
+   if ( document.querySelector(".style-switchers").classList.contains("open"))
+   {
+        document.querySelector(".style-switchers").classList.remove("open")
+   }
+     
+})
+
+//===================theme COLOR=================================
+const alternateStyle = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color)
+{
+    alternateStyle.forEach((style) => {
+        if(color === style.getAttribute("title")){
+            style.removeAttribute("disabled");
+        }
+        else{
+            style.setAttribute("disabled","true");
+        }
+    })
+}
+
+//===================theme light and dark mode=================================
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle('fa-sun');
+    dayNight.querySelector("i").classList.toggle('fa-moon');
+    document.body.classList.toggle("dark");
+})
+
+window.addEventListener("load", () => {
+    if(document.body.classList.contains("dark"))
+    {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else{
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+})
+
